63f0e9bda44e5343e127b5c091623b3513ee8d0f
Adrian Hristov <adrian@intelligentgrowthsolutions.com>
2024-04-28 16:27:13 +0100

    initial astro project

---
+++ astro.config.mjs
@@ -0,0 +1,18 @@
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import tailwind from "@astrojs/tailwind";
+
+export default defineConfig({
+  site: "https://astro-nano-demo.vercel.app",
+  integrations: [mdx(), sitemap(), tailwind()],
+  image: {
+    // Example: Enable the Sharp-based image service with a custom config
+    service: {
+       entrypoint: 'astro/assets/services/sharp',
+       config: {
+         limitInputPixels: false,
+      },
+     },
+  }
+});
---
+++ src/content/projects/blair-atholl/index.md
@@ -0,0 +1,8 @@
+---
+title: "Blair Atholl"
+description: "A solo trip to Blair Atholl"
+date: "Apr 26 2024"
+---
+![Cow](/IMG_0066.JPG)
+
+![Bird](./bird.JPG)
